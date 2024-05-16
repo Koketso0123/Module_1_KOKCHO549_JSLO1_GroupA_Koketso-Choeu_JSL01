8e1841c0c0e1a3400b03b9d8b95a34f1da12a6b5
Koketso Choeu <chueukeutlwilekoketso@gmail.com>
2024-05-16 21:33:40 +0200

    commit

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
+
 function validateSyntax() {
-    let input = document.getElementById('petInput').value;
+    let input = document.getElementById("petInput").value;
     // Validation logic goes here
+    const petCheck = /^pet_[a-zA-Z0-9]+$/;
     let result = ''; // Placeholder for validation result
 
     // TODO: Write your validation logic here
         // Check if input starts with 'pet_' and followed by alphanumeric characters
-
-            document.getElementById('result').innerText = result;
+if( petCheck.test(input)){
+    document.getElementById("result").textContent = "Valid Input";
+} else{
+    document.getElementById("result").textContent = "Invalid Input";
+}
+            // document.getElementById('result').innerText = result;
+}
+//This function will clear the input field and result area to make it more user friendly  
+function clearInput(){
+    // this line turns the value inside the input field to an empty string
+    document.getElementById("petInput").value = "";
+     // this line turns the text of the result  to an empty string
+    document.getElementById("result").textContent = "";
 }
 
 
